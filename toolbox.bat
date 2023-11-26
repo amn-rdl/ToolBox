bcd734d9428c7395294b3a8d5fb6ac46c4242105
AMN <127019608+amn-rdl@users.noreply.github.com>
2023-11-26 17:56:59 +0100

    Add files via upload

---
+++ toolbox.bat
@@ -0,0 +1,13 @@
+cls
+@echo off
+echo.
+echo ToolBox Launcher       -       AMN Corp.
+echo.
+echo *****************************************
+echo.
+pause
+echo.
+echo.
+@echo off
+cd C:\Users\yorisamani\Documents\Perso\VS Code\ToolBox
+python toolbox.py
